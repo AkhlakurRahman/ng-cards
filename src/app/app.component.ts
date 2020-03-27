cda74cd6ef48b11b9981c2ec2f5b0109f2ec199f
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-03-27 15:30:42 +0600

    card component dynamically added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'ng-cards';
+  posts = [
+    {
+      title: "Tree in Nature",
+      imageUrl: "assets/tree.jpeg",
+      username: "@nature",
+      content: "Tree in Nature. What a beautiful sight!"
+    },
+    {
+      title: "Snowy Mountain",
+      imageUrl: "assets/mountain.jpeg",
+      username: "@mountain",
+      content: "Snowy mountain in winter. What a beautiful sight!"
+    },
+    {
+      title: "Mountain Biking",
+      imageUrl: "assets/biking.jpeg",
+      username: "@biking",
+      content: "Mountain Biking. What a brave person"
+    }
+  ];
 }
